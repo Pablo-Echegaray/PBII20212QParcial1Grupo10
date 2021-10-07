94ed3fc6a0932daa5b667b737f5d55cdff0caae8
Pablo <pabloechegaray95@gmail.com>
2021-10-06 23:46:39 -0300

    Se crean las clases a desarrollar

---
+++ src/ar/edu/unlam/dominio/Genero.java
@@ -0,0 +1,5 @@
+package ar.edu.unlam.dominio;
+
+public enum Genero {
+TERROR, NOVELA, CUENTOS, CIENCIA_FICCION, ACADEMICO;
+}
---
+++ testJunit/ar/edu/unlam/testJunit/testLibreria.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.testJunit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testLibreria {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
