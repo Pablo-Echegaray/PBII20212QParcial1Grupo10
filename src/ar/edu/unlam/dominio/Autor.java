77a7e0c6c7419a5407fdcf11220624b4b0076f03
Laura Huayhua Vargas <70HU41707713@campus.economicas.uba.ar>
2021-10-08 20:24:04 -0300

    agregando clase autor

--- src/ar/edu/unlam/dominio/Autor.java
+++ src/ar/edu/unlam/dominio/Autor.java
@@ -1,9 +1,33 @@
 package ar.edu.unlam.dominio;
 
 public class Autor extends Persona{
-
+	private  Libro [] librosEscritos;
+	private Integer contadorLibros;
+	
 	public Autor(String nombre, String nacionalidad, Integer edad, String domicilio, Integer dni) {
 		super(nombre, nacionalidad, edad, domicilio, dni);
+		
+	}
+	public Libro[] getLibrosEscritos() {
+		return librosEscritos;
+	}
+
+	public void setLibrosEscritos(Libro[] librosEscritos) {
+		this.librosEscritos = librosEscritos;
+	}
+	
+	
+	public Boolean agregarLibro(Libro librosEscrito) {
+		Boolean seAgregoLibro=false;
+		
+		for(int i = 0; i < librosEscritos.length; i++) {
+			if(librosEscritos[i] == null) {
+				librosEscritos [contadorLibros++] = librosEscrito;
+				seAgregoLibro=true;
+				break;
+			}
+		}
+		return seAgregoLibro;
 		
 	}
 
